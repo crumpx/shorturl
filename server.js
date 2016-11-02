88d32f846a1fe4c06dca9fdafd66a641dd622852
crumpx <crumpx@gmail.com>
2016-11-02 16:27:08 -0400

    add db

--- server.js
+++ server.js
@@ -1,44 +1,84 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var port = process.env.PORT || 8080
-var adds = {};
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
+var adds = {};
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'public'));
 app.use(express.static(__dirname + '/public'));
 
+var db;
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, database){
+	if (err) {
+		console.log(err);
+		process.exit(1);
+	}
+	db = database;
+	console.log("Database connection ready");
+
+	var server = app.listen(process.env.PORT || 8080, function(){
+		var port = server.address().port;
+		console.log('Server running');
+	});
+});
+
+
+
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+
 app.get('/', function(req, res){
 	var host = req.headers.host;
-	res.render('index', {adds: adds, host: host});
-	
+	var urls = db.collection('urls');
+	urls.find({}).toArray(function(err, doc){
+		if (err) throw err;
+		res.render('index', {adds: doc, host: host});
+	});
 });
 
 app.get('/:id', function(req, res){
 	var id = req.params.id;
-	if (adds !== {} && adds[id] !== undefined){
-		res.redirect(adds[id][1])
-	} else {
-		var host = req.headers.host;
-		res.send('NO such shorten address!<br /><a href="http://'
+	var host = req.headers.host;
+	var urls = db.collection('urls');
+
+	urls.findOne({short_url:{$regex:id}}).then(function(url){
+		if (url === null) {
+			var host = req.headers.host;
+			res.send('NO such shorten address!<br /><a href="http://'
 			+req.headers.host+
 			'/">Home Page</a>');
-	}
+		} else {
+			res.redirect(url.original_url+'/');
+		}
+	})
 });
+	
 
 app.get('/short/*', function(req, res){
 	var result = {};
 	var original = req.params[0];
 	var shorten = Math.floor(Math.random() * 10000) + 1;
-	adds[shorten] = [shorten, original];
 	result.original_url = original;
 	result.short_url = req.headers.host+'/'+shorten;
-	res.send(JSON.stringify(result)/*
-		+'<br><h2>shorten url:</h2><a href="http://'
-		+result.short_url
-		+'/">'+result.short_url+'</a><br><br><a href="http://'+req.headers.host+'/">Home Page</a>'*/);
+	var urls = db.collection('urls');
+
+	urls.findOne({original_url:original}).then(function(url){
+		if (url == null) {
+			urls.insert(result, function(err, data){
+				if (err) console.log(err);
+				delete data.ops[0]._id;
+				res.send(JSON.stringify(data.ops[0]));
+			})
+		} else {
+			delete url._id;
+			res.send(JSON.stringify(url));
+		}
+	})
 });
 
-app.listen(port, function(){
-	console.log('Server running');
-});
